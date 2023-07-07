dbf3d7c50e87b519d326cd0f00c50b47284eb8bd
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-07 11:22:37 +0300

    Connecting to database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import userRoutes from './routes/userRoutes.js';
+import connectDB from './config/db.js';
 import { notFound,errorHandler } from './middleware/errorMiddleware.js';
 
 dotenv.config();
@@ -8,6 +9,7 @@ const port = process.env.PORT || 5000;
 
 const app = express();
 
+connectDB();
 app.use('/api/users', userRoutes);
 app.get('/', (req,res) => res.send("Server is ready!"));
 
