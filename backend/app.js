683c89266fc276fd3b4715664f5c5ab02734dc93
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-07 10:02:08 +0300

    Setting up user routes and controllers

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 
-app.get('/', (req,res) => res.send("Server is ready!"))
+app.get('/', (req,res) => res.send("Server is ready!"));
+app.use('/api/users', userRoutes)
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
---
+++ backend/controllers/userControllers.js
@@ -0,0 +1,6 @@
+//Auth user /set token
+//route POST /api/users/auth
+//@access Public
+export const authUser = (req,res) => {
+  res.status(200).json({message: 'Auth User'})
+}
