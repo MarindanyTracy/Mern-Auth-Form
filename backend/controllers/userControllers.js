c813ab12540e65766f00b5fcd4921573844d9179
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-07 10:34:11 +0300

    Custom Error Middleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,6 +1,9 @@
+import asyncHandler from 'express-async-handler';
+
 //Auth user /set token
 //route POST /api/users/auth
 //@access Public
-export const authUser = (req,res) => {
+export const authUser = asyncHandler(async(req,res) => {
+
   res.status(200).json({message: 'Auth User'})
-}
+});
