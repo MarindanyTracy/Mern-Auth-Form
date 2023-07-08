42d92d25f5a29867531c3830338a0a445fcff533
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-08 21:27:04 +0300

    Auth User Endpoint

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -3,6 +3,20 @@ import generateToken from '../utils/generateToken.js';
 import User from "../models/userModels.js";
 
 export const authUser = asyncHandler(async(req,res) => {
+  const { email, password} = req.body;
+  const user = await User.findOne({email});
+
+  if(user && (await user.matchPasswords(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({ 
+      _id:user._id,
+      name: user.name,
+      email: user.email
+    })
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password!')
+  }
 
   res.status(200).json({message: 'Auth User'})
 });
