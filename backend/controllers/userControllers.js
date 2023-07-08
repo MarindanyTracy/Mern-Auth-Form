87ea30b90e98d2d482078c516e09324d2e3d3020
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-08 21:31:08 +0300

    Destroying cookie and logging out

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -50,8 +50,11 @@ export const registerUser = asyncHandler(async(req,res) => {
 });
 
 export const logoutUser = asyncHandler(async(req,res) => {
-
-  res.status(200).json({message: 'Logout User'})
+  res.cookie('jwt', '', {
+    httpOnly: true,
+    expires: new Date(0)
+  })
+  res.status(200).json({message: 'User Logged Out'})
 });
 
 export const getUserProfile = asyncHandler(async(req,res) => {
