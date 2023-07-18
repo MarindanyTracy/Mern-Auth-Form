6e83af4a8dd258c0967b7abd6d26b35abe00083d
MarindanyTracy <marindanytracy0@gmail.com>
2023-07-18 16:37:32 +0300

    Register functionality

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,8 +4,8 @@ import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
+router.post('/' , registerUser);
 router.post('/auth' , authUser);
-router.post('/register' , registerUser);
 router.post('/logout' , logoutUser);
 router.route('/profile').get(protect, getUserProfile).put(protect, updateUserProfile);
 
